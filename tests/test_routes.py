887a8b7d8ef05f4cca2c8c3151b6cfc9032f2a32
Henson-Kudi <amahkudi@gmail.com>
2022-12-28 20:09:37 -0500

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -127,7 +127,7 @@ class TestAccountService(TestCase):
 
     def test_read_an_account(self):
         """It should Read a single Account"""
-        account = self._create_accounts(1)[]
+        account = self._create_accounts(1)[0]
 
         response = self.client.get(
             f"{BASE_URL}/{account.id}", content_type="application/json"
